ee028535fc2386086a4241c39c648645031a33e9
ningyongqiang <15901754062@163.com>
2021-02-02 13:46:44 +0800

    CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   mode: 'development',
@@ -18,6 +19,11 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "public", to: "public" }
+      ],
     })
   ],
   module: {
